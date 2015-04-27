c449e511f2e1014bc61214975cbf7afb543a9f57
Asim Raza Khan <asim.r.k@hotmail.com>
2015-04-28 02:12:28 +0500

    marge the review.js with app.js every thing is working

--- app.js
+++ app.js
@@ -149,6 +149,33 @@
 })();
 
 
+(function(){
+    angular.module('reviewModule', ['ngMaterial', 'firebase'])
+        .controller('reviewController', function($firebaseArray) {
+            this.userReviews = new Firebase("https://tdo.firebaseio.com/UserReviews");
+            this.reviews = $firebaseArray(this.userReviews);
+
+            this.message = {
+                stars: 5
+            };
+
+            this.post = function () {
+
+                if (this.message) {
+
+                    // adding the object into the array index (that is currently on in angular repeat)
+                    this.reviews.$add(this.message);
+                    this.message = {
+                        stars: 5
+                    };
+                }
+            };
+
+
+        });
+}) ();
+
+
 
 
 
